021aac91820de230280206243c6a76243bb211e6
GerezPablo <pablogerez2000@gmail.com>
2020-05-20 20:21:01 -0300

    Update

--- Clase N°4/insercion-y-actualizacion-varios.js
+++ Clase N°4/insercion-y-actualizacion-varios.js
@@ -12,36 +12,45 @@ class User extends Sequelize.Model {}
 User.init({
     firstName: Sequelize.STRING,
     lastName: Sequelize.STRING, 
-    dni: { type: Sequelize.STRING, allowNull: false } 
+    age: Sequelize.INTEGER,
+    dni: { type: Sequelize.INTEGER, allowNull: false } 
     }, { sequelize, modelName: 'user' });
 
 sequelize.sync()
 .then(() => User.create({
     firstName: 'Pavlov',
     lastName: 'Gerez',
+    age: 20,
     dni: '42424242'
 }))
 .then(() => User.create({
     firstName: 'Gerardo',
     lastName: 'Ford',
+    age: 40,
     dni: '56345364'
 }))
 .then(() => User.create({
     firstName: 'Ricardo',
     lastName: 'Nixon',
+    age:  56,
     dni: '12424524'
 }))
 .then(() => User.create({
     firstName: 'Petruccio',
     lastName: 'Auditore',
+    age: 12,
     dni: '42424242'
 }))
 .then(jane => {
     console.log(jane.toJSON());
 })
+
 .then(() =>  //Actualiza 
     User.update( {firstName: "Enrique"}, {
-        where: { 
-            dni: '4%'}
+        where: {
+            dni: {
+              [Sequelize.like]: '4%'
+            }
+        }
     })
 .then(() => { console.log("Done"); }) );

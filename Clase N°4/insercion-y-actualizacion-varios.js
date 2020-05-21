9383ebbf710c85dc4cd52ee4bea47f4c08bd25bf
GerezPablo <pablogerez2000@gmail.com>
2020-05-20 21:57:02 -0300

    Clase4 Funcionando

--- Clase N°4/insercion-y-actualizacion-varios.js
+++ Clase N°4/insercion-y-actualizacion-varios.js
@@ -1,21 +1,18 @@
 const Sequelize = require('sequelize');
-
+const Op = Sequelize.Op;
 const sequelize = new Sequelize('clase4', 'root', '', { host: 'localhost', dialect: 'mariadb' });
-
 sequelize
   .authenticate()
-  .then(() => { console.log('Todo Correcto.'); }) 
+  .then(() => { console.log('Todo Correcto.'); })
   .catch(err => { console.error(`Che paso esto: ${err}. Fijate que onda.`); });
-
 
 class User extends Sequelize.Model {}
 User.init({
     firstName: Sequelize.STRING,
-    lastName: Sequelize.STRING, 
+    lastName: Sequelize.STRING,
     age: Sequelize.INTEGER,
-    dni: { type: Sequelize.INTEGER, allowNull: false } 
+    dni: { type: Sequelize.INTEGER, allowNull: false }
     }, { sequelize, modelName: 'user' });
-
 sequelize.sync()
 .then(() => User.create({
     firstName: 'Pavlov',
@@ -39,18 +36,15 @@ sequelize.sync()
     firstName: 'Petruccio',
     lastName: 'Auditore',
     age: 12,
-    dni: '42424242'
+    dni: '42424243'
 }))
 .then(jane => {
     console.log(jane.toJSON());
 })
-
-.then(() =>  //Actualiza 
+.then(() =>  //Actualiza
     User.update( {firstName: "Enrique"}, {
         where: {
-            dni: {
-              [Sequelize.like]: '4%'
-            }
+            dni: { [Op.like]: '4%'}
         }
     })
 .then(() => { console.log("Done"); }) );
